fea8aa1755adc2e8f7774a9d03fff6d01a5f09cb
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-01-20 07:55:43 -0700

    working sidebar and navbar

--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
-from datetime import date, datetime, timedelta
-import dataretrieval.nwis as nwis
-import utils.param_codes as pc
 import dash
 from dash import html, dcc, Input, Output, State
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
+import dataretrieval.nwis as nwis
+import utils.param_codes as pc
 import plotly.graph_objects as go
 import pandas as pd
 from utils import utils
 import json
+from datetime import date, datetime, timedelta
 
-# import dash_bootstrap_components as dbc
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
 STAID_coord = pd.read_csv("data/JHA_STAID_INFO.csv")
 mapbox_access_token = "pk.eyJ1Ijoic2xlZXB5Y2F0IiwiYSI6ImNsOXhiZng3cDA4cmkzdnFhOWhxdDEwOHQifQ.SU3dYPdC5aFVgOJWGzjq2w"
 
@@ -43,98 +44,53 @@ def create_map(lat="dec_lat_va", long="dec_long_va"):
     return fig
 
 
+navbar = dbc.Navbar(
+    [
+        dbc.NavbarBrand("Test"),
+        dbc.Nav([dbc.NavLink("Item 1"), dbc.NavLink("Item 2")]),
+    ],
+    sticky="top",
+    color="dark",
+    dark=True,
+    style={"width": "100%"},
+)
+# the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",
-    "top": 0,
+    "top": "2rem",
     "left": 0,
     "bottom": 0,
-    "width": "18rem",
-    "padding": "4rem 1rem 2rem",
+    "width": "16rem",
+    "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
 
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
 CONTENT_STYLE = {
-    "margin-left": "19rem",
+    "margin-left": "18rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
 }
-
-
-# app = dash.Dash(
-#     __name__,
-#     external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
-#     # these meta_tags ensure content is scaled correctly on different devices
-#     # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
-#     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-# )
-
-app = dash.Dash(
-    __name__,
-    suppress_callback_exceptions=True,
-    external_stylesheets=[dbc.themes.MATERIA, dbc.icons.FONT_AWESOME],
-)
-
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.Button("Sidebar", outline=True, color="secondary", className="mr-1", id="btn_sidebar"),
-        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("More pages", header=True),
-                dbc.DropdownMenuItem("Page 2", href="#"),
-                dbc.DropdownMenuItem("Page 3", href="#"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
-    ],
-    brand="Brand",
-    brand_href="#",
-    color="dark",
-    dark=True,
-    fluid=True,
-)
 
 sidebar = html.Div(
     [
-        html.Div(
-            [
-                html.H2("Auto ML", style={"color": "white"}),
-            ],
-            className="sidebar-header",
-        ),
+        html.H2("Sidebar", className="display-4"),
         html.Hr(),
+        html.P("A simple sidebar layout with navigation links", className="lead"),
         dbc.Nav(
             [
-                dbc.NavLink(
-                    [html.I(className="fas fa-home me-2"), html.Span("Dashboard")],
-                    href="/",
-                    active="exact",
-                ),
-                dbc.NavLink(
-                    [
-                        html.I(className="fas fa-calendar-alt me-2"),
-                        html.Span("Projects"),
-                    ],
-                    href="/projects",
-                    active="exact",
-                ),
-                dbc.NavLink(
-                    [
-                        html.I(className="fas fa-envelope-open-text me-2"),
-                        html.Span("Datasets"),
-                    ],
-                    href="/datasets",
-                    active="exact",
-                ),
+                dbc.NavLink("Page 1", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Page 2", href="/page-2", id="page-2-link"),
+                dbc.NavLink("Page 3", href="/page-3", id="page-3-link"),
             ],
             vertical=True,
             pills=True,
         ),
         html.Div(
             [
-                "Station ID: ",
+                html.P("Station ID: "),
                 dcc.Dropdown(
                     id="station_ID",
                     value="433641110441501",
@@ -163,82 +119,105 @@ sidebar = html.Div(
             ],
         ),
     ],
-    className="sidebar",
+    id="sidebar",
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(
+    [
+        html.H1(
+            id="H1",
+            children="The QCinator, it's coming for your data!",
+            style={"textAlign": "center", "marginTop": 40, "marginBottom": 40},
+        ),
+        dcc.Graph(id="location_map", figure=create_map()),
+        html.Div(
+            [
+                "Select parameter by name: ",
+                dcc.Dropdown(
+                    id="param_select",
+                    options=pc.param_labels,
+                    value="p00400",
+                ),
+            ],
+            style={"width": "49%", "display": "inline-block"},
+        ),
+        dcc.Graph(id="scatter_plot"),
+        html.Div(
+            [
+                "Select X axis parameter: ",
+                dcc.Dropdown(
+                    id="param_select_X",
+                    options=pc.param_labels,
+                    value="p00400",
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                "Select Y axis parameter: ",
+                dcc.Dropdown(
+                    id="param_select_Y",
+                    options=pc.param_labels,
+                    value="p00400",
+                ),
+            ]
+        ),
+        dcc.Graph(id="plot_X_vs_Y", style={"display": "inline-block"}),
+        dcc.Store(id="memory_data", storage_type="memory"),
+        dcc.Store(id="filtered_data", storage_type="memory"),
+        dcc.Store(id="STAID", storage_type="memory", data="12323840"),
+    ],
+    id="page-content",
+    style=CONTENT_STYLE,
 )
 
 application = app.server
 
 app.layout = html.Div(
     [
+        # dcc.Store(id="side_click"),
+        dcc.Location(id="url"),
         navbar,
-        sidebar,
         html.Div(
             [
-                html.H1(
-                    id="H1",
-                    children="The QCinator, it's coming for your data!",
-                    style={"textAlign": "center", "marginTop": 40, "marginBottom": 40},
-                ),
-                dcc.Graph(id="location_map", figure=create_map()),
-                html.Div(
-                    [
-                        "Select parameter by name: ",
-                        dcc.Dropdown(
-                            id="param_select",
-                            options=pc.param_labels,
-                            value="p00400",
-                        ),
-                    ],
-                    style={"width": "49%", "display": "inline-block"},
-                ),
-                dcc.Graph(id="scatter_plot"),
-                html.Div(
-                    [
-                        "Select X axis parameter: ",
-                        dcc.Dropdown(
-                            id="param_select_X",
-                            options=pc.param_labels,
-                            value="p00400",
-                        ),
-                    ]
-                ),
-                html.Div(
-                    [
-                        "Select Y axis parameter: ",
-                        dcc.Dropdown(
-                            id="param_select_Y",
-                            options=pc.param_labels,
-                            value="p00400",
-                        ),
-                    ]
-                ),
-                dcc.Graph(id="plot_X_vs_Y", style={"display": "inline-block"}),
-                dcc.Store(id="memory_data", storage_type="memory"),
-                dcc.Store(id="filtered_data", storage_type="memory"),
-                dcc.Store(id="STAID", storage_type="memory", data="12323840"),
+                sidebar,
+                content,
             ],
-            className="content",
         ),
     ]
 )
 
 
-# @app.callback(Output("location_map", "figure"), Input("location_map", "ClickData"))
-# def display_click_data(clickData):
-#     return json.dumps(clickData, indent=2)
-
-
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
 @app.callback(
-    Output("STAID", "value"),
-    Output("station_ID", "options"),
-    [
-        Input("location_map", "clickData"),
-    ],
+    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
+    [Input("url", "pathname")],
 )
-def update_STAID(clickData):
-    if not clickData:
-        raise PreventUpdate
-    return [str(clickData["points"][0]["customdata"])]
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 4)]
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return content
+    elif pathname == "/page-2":
+        return html.P("This is the content of page 2. Yay!")
+    elif pathname == "/page-3":
+        return html.P("Oh cool, this is page 3!")
+    # If the user tries to reach a different page, return a 404 message
+    return html.Div(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
 
 
 @app.callback(
@@ -334,29 +313,3 @@ def x_vs_y(station, param_x: str, param_y: str, data):
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-
-
-# html.Div(
-#     [
-#         "Filter by sample type: ",
-#         dcc.Dropdown(
-#             id="sample_code_select",
-#             options=pc.sample_codes,
-#             value="9",
-#         ),
-#     ],
-#     style={"width": "20%"},
-# ),
-
-
-# @app.callback(
-#     Output("filtered_data", "data"),
-#     [
-#         Input("sample_code_select", "value"),
-#         Input("memory_data", "value"),
-#     ],
-# )
-# def filter_qw_data(sample_code, data):
-#     df = pd.read_json(data)
-#     df = df.loc[df["samp_type_cd"] == sample_code]
-#     return df.to_json()
