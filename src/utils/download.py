b81ace6ecb803aff65dc6bb9647f71349a4f7c13
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-06-26 14:39:08 -0600

    get_meta_data()

--- src/utils/download.py
+++ src/utils/download.py
@@ -1,17 +1,14 @@
-import dash
-from dash import html, dcc, callback, Input, Output
-from datetime import date
-import dataretrieval.nwis as nwis
+import pandas as pd
+from typing import List
 
 
-# @callback(
-#     Output("memory_data", "data"),
-#     [
-#         Input("station_ID", "value"),
-#         Input("date_range", "start_date"),
-#         Input("date_range", "end_date"),
-#     ],
-# )
-# def get_qw_data(site, start, end):
-#     df = nwis.get_record(sites=site, service="qwdata", start=start, end=end, access="3")
-#     return df.to_json()
+def get_meta_data(staids: List) -> pd.DataFrame:
+    # staids = [433615110440001, 433600110443701, 433604110443402]
+    staid_str = ",".join(str(staid) for staid in staids)
+    url = f"https://waterservices.usgs.gov/nwis/site/?format=rdb&sites={staid_str}&siteOutput=expanded&siteStatus=all"
+    # s=requests.get(url).text
+    dataframe = pd.read_csv(url, sep="\t", comment="#")
+    dataframe = dataframe.drop(index=0)
+    dataframe = dataframe.rename(columns={"site_no": "staid"})
+    dataframe[["station_str", "station_name"]] = dataframe["station_nm"].str.split(" ").apply(pd.Series)
+    return dataframe[["staid", "station_name", "dec_lat_va", "dec_long_va"]]
