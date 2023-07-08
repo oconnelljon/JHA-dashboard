c4c661f86901b903547e40f22663566f28f76d9d
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-07-08 16:34:49 -0600

    able to set port and address now

--- src/app.py
+++ src/app.py
@@ -2,14 +2,18 @@
 import dash
 import dash_bootstrap_components as dbc
 
+from utils.settings import PORT, ADDRESS
 import utils.data
 import utils.callbacks
 from components.layout import make_layout
+import flask
 
+
+f_app = flask.Flask(__name__)
 # Initialize App
-app = dash.Dash(__name__, assets_folder="assets", external_stylesheets=[dbc.themes.SPACELAB], title="JHA Dashboard")  # Include __name__, serves as reference for finding .css files.
-server = app.server  # Important for debugging and using Flask!
+app = dash.Dash(__name__, assets_folder="assets", external_stylesheets=[dbc.themes.SPACELAB], title="JHA Dashboard", server=f_app)  # Include __name__, serves as reference for finding .css files.
+# server = app.server  # Important for debugging and using Flask!
 app.layout = make_layout()
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=False, port=PORT, host=ADDRESS)
