9ae4ed38f735236e675f9ca219b8aedb64a3dd2a
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-07-13 15:55:57 -0600

    breakup layout

--- src/components/layout.py
+++ src/components/layout.py
@@ -1,6 +1,8 @@
 from dash import dcc, html, dash_table
+import dash_bootstrap_components as dbc
 from components import main_navbar, main_sidebar
 from utils import data
+from utils.settings import DEFAULT_PCODE
 
 parameter_of_interest_text = "Select a parameter of interest to populate the Time-Series and Box plots.  Only selected stations and data within the time range are displayed."
 time_plot_text = "The Time-Series Plot shows the values for the Parameter of Interest for the selected stations across the selected time range."
@@ -8,6 +10,174 @@ box_plot_text = "Box plots show the distribution of data for the entire selected
 scatter_x_y_text = "The scatter plot displays data from the above drop down menues on their respective axis'. Only selected stations and data within the time range are displayed."
 scatter_x_y_z_text = "Plot X vs Y data with a third parameter that controls the size of the plot marker."
 summary_table_text = "The Summary Table contains general information about the selected stations in the time range for the Parameter of Interest.  Stations with no data are not displayed."
+
+
+poi_div = html.Div(
+    [
+        html.H1("Parameter of interest"),
+        html.Div(
+            [
+                dcc.Dropdown(
+                    id="param_select",
+                    options=data.available_param_dict,
+                    value=DEFAULT_PCODE,
+                    persistence=True,
+                    clearable=False,
+                ),
+            ],
+            id="main-parameter-dropdown",
+        ),
+        dbc.Card(
+            [
+                dbc.CardHeader("Time-Series Plot"),
+                dcc.Graph(id="plot_param_ts", className="scatter-plot"),
+                dbc.CardHeader(
+                    time_plot_text,
+                    # className="plot-text",
+                ),
+            ],
+            className="plots-wrapper",
+        ),
+        dbc.Card(
+            [
+                dbc.CardHeader("Box Plot"),
+                dcc.Graph(id="plot_box", className="box-plot"),
+                dbc.CardHeader(
+                    box_plot_text,
+                    # className="plot-text",
+                ),
+            ],
+            className="plots-wrapper",
+        ),
+        dbc.Card(
+            [
+                dbc.CardHeader(id="data-table-text"),
+                dash_table.DataTable(
+                    id="summary-table",
+                    sort_action="native",
+                ),
+                dbc.CardHeader(
+                    summary_table_text,
+                    # className="plot-text",
+                ),
+            ],
+            className="table-container",
+        ),
+    ],
+    className="tile-container",
+)
+
+comp_xy_div = dbc.Card(
+    [
+        html.Div(
+            [
+                dbc.CardHeader("Scatter X parameter"),
+                dcc.Dropdown(
+                    id="param_select_x",
+                    options=data.available_param_dict,
+                    value="p00400",
+                    clearable=False,
+                ),
+            ],
+            className="main-content-dropdown",
+            id="select-x-container",
+        ),
+        html.Div(
+            [
+                dbc.CardHeader("Scatter Y parameter"),
+                dcc.Dropdown(
+                    id="param_select_y",
+                    options=data.available_param_dict,
+                    value="p00300",
+                    clearable=False,
+                ),
+            ],
+            className="main-content-dropdown",
+            id="select-y-container",
+        ),
+        dcc.Graph(id="plot_xy"),
+        dbc.CardHeader(
+            scatter_x_y_text,
+            # className="plot-text",
+        ),
+    ],
+    className="plots-wrapper",
+)
+
+comp_xyz_div = dbc.Card(
+    [
+        html.Div(
+            [
+                dbc.CardHeader("Scatter X parameter"),
+                dcc.Dropdown(
+                    id="param_select_xx",
+                    options=data.available_param_dict,
+                    value="p00400",
+                    clearable=False,
+                ),
+            ],
+            className="main-content-dropdown",
+            id="select-xx-container",
+        ),
+        html.Div(
+            [
+                dbc.CardHeader("Scatter Y parameter"),
+                dcc.Dropdown(
+                    id="param_select_yy",
+                    options=data.available_param_dict,
+                    value="p00300",
+                    clearable=False,
+                ),
+            ],
+            className="main-content-dropdown",
+            id="select-yy-container",
+        ),
+        html.Div(
+            [
+                dbc.CardHeader("Bubble size parameter"),
+                dcc.Dropdown(
+                    id="param_select_zz",
+                    options=data.available_param_dict,
+                    value="p00095",
+                    clearable=False,
+                ),
+            ],
+            className="main-content-dropdown",
+            id="select-zz-container",
+        ),
+        dcc.Graph(id="plot_xyz"),
+        dbc.CardHeader(
+            scatter_x_y_z_text,
+            # className="plot-text",
+        ),
+    ],
+    className="plots-wrapper",
+)
+
+main_div = html.Div(
+    [
+        html.Main(
+            [
+                html.Div(
+                    [
+                        poi_div,
+                        html.Div(
+                            [
+                                html.H1("Comparative Plots"),
+                                comp_xy_div,
+                                comp_xyz_div,
+                            ],
+                            className="tile-container",
+                        ),
+                    ],
+                    className="main-content-container",
+                ),
+            ],
+            className="main-body-container",
+        ),
+    ],
+    id="main-wrapper",
+)
 
 
 def make_layout():
@@ -23,161 +193,7 @@ def make_layout():
                     html.Div(
                         [
                             main_sidebar(checklist_option=data.STATION_NMs),
-                            html.Main(
-                                [
-                                    html.Div(
-                                        [
-                                            html.Div(
-                                                [
-                                                    html.H1("Parameter of interest"),
-                                                    html.Div(
-                                                        [
-                                                            dcc.Dropdown(
-                                                                id="param_select",
-                                                                options=data.available_param_dict,
-                                                                value=data.DEFAULT_PCODE,
-                                                                persistence=True,
-                                                                clearable=False,
-                                                            ),
-                                                        ],
-                                                        id="main-parameter-dropdown",
-                                                    ),
-                                                    html.Div(
-                                                        [
-                                                            html.H2("Time-Series Plot"),
-                                                            dcc.Graph(id="plot_param_ts", className="scatter-plot"),
-                                                            html.P(
-                                                                time_plot_text,
-                                                                className="plot-text",
-                                                            ),
-                                                        ],
-                                                        className="plots-wrapper",
-                                                    ),
-                                                    html.Div(
-                                                        [
-                                                            html.H2("Box Plot"),
-                                                            dcc.Graph(id="plot_box", className="box-plot"),
-                                                            html.P(
-                                                                box_plot_text,
-                                                                className="plot-text",
-                                                            ),
-                                                        ],
-                                                        className="plots-wrapper",
-                                                    ),
-                                                    html.Div(
-                                                        [
-                                                            html.H2(id="data-table-text"),
-                                                            dash_table.DataTable(
-                                                                id="summary-table",
-                                                                sort_action="native",
-                                                            ),
-                                                            html.P(
-                                                                summary_table_text,
-                                                                className="plot-text",
-                                                            ),
-                                                        ],
-                                                        className="table-container",
-                                                    ),
-                                                ],
-                                                className="tile-container",
-                                            ),
-                                            html.Div(
-                                                [
-                                                    html.H1("Comparative Plots"),
-                                                    html.Div(
-                                                        [
-                                                            html.Div(
-                                                                [
-                                                                    html.H2("Scatter X parameter"),
-                                                                    dcc.Dropdown(
-                                                                        id="param_select_x",
-                                                                        options=data.available_param_dict,
-                                                                        value="p00400",
-                                                                        clearable=False,
-                                                                    ),
-                                                                ],
-                                                                className="main-content-dropdown",
-                                                                id="select-x-container",
-                                                            ),
-                                                            html.Div(
-                                                                [
-                                                                    html.H2("Scatter Y parameter"),
-                                                                    dcc.Dropdown(
-                                                                        id="param_select_y",
-                                                                        options=data.available_param_dict,
-                                                                        value="p00300",
-                                                                        clearable=False,
-                                                                    ),
-                                                                ],
-                                                                className="main-content-dropdown",
-                                                                id="select-y-container",
-                                                            ),
-                                                            dcc.Graph(id="plot_xy"),
-                                                            html.P(
-                                                                scatter_x_y_text,
-                                                                className="plot-text",
-                                                            ),
-                                                        ],
-                                                        className="plots-wrapper",
-                                                    ),
-                                                    html.Div(
-                                                        [
-                                                            html.Div(
-                                                                [
-                                                                    html.H2("Scatter X parameter"),
-                                                                    dcc.Dropdown(
-                                                                        id="param_select_xx",
-                                                                        options=data.available_param_dict,
-                                                                        value="p00400",
-                                                                        clearable=False,
-                                                                    ),
-                                                                ],
-                                                                className="main-content-dropdown",
-                                                                id="select-xx-container",
-                                                            ),
-                                                            html.Div(
-                                                                [
-                                                                    html.H2("Scatter Y parameter"),
-                                                                    dcc.Dropdown(
-                                                                        id="param_select_yy",
-                                                                        options=data.available_param_dict,
-                                                                        value="p00300",
-                                                                        clearable=False,
-                                                                    ),
-                                                                ],
-                                                                className="main-content-dropdown",
-                                                                id="select-yy-container",
-                                                            ),
-                                                            html.Div(
-                                                                [
-                                                                    html.H2("Bubble size parameter"),
-                                                                    dcc.Dropdown(
-                                                                        id="param_select_zz",
-                                                                        options=data.available_param_dict,
-                                                                        value="p00095",
-                                                                        clearable=False,
-                                                                    ),
-                                                                ],
-                                                                className="main-content-dropdown",
-                                                                id="select-zz-container",
-                                                            ),
-                                                            dcc.Graph(id="plot_xyz"),
-                                                            html.P(
-                                                                scatter_x_y_z_text,
-                                                                className="plot-text",
-                                                            ),
-                                                        ],
-                                                        className="plots-wrapper",
-                                                    ),
-                                                ],
-                                                className="tile-container",
-                                            ),
-                                        ],
-                                        className="main-content-container",
-                                    ),
-                                ],
-                                className="main-body-container",
-                            ),
+                            main_div,
                         ],
                         className="body-container",
                     ),
