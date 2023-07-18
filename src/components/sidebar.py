e2fcc7ee8538fcdda7e800bb9c8e75ee9344db47
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-07-18 11:17:23 -0600

    heading change

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -8,7 +8,7 @@ def main_sidebar(checklist_option):
         [
             html.Div(
                 [
-                    html.H1("Station ID"),
+                    html.H2("Station ID"),
                     dcc.Checklist(["All"], ["All"], id="all-checklist", inline=True),
                     dcc.Checklist(
                         id="station-checklist",
@@ -21,7 +21,7 @@ def main_sidebar(checklist_option):
             ),
             html.Div(
                 [
-                    html.H1("Select Date Range"),
+                    html.H2("Select Date Range"),
                     dcc.DatePickerRange(
                         id="date_range",
                         start_date=datetime.now().date() - timedelta(days=1460),
@@ -39,7 +39,7 @@ def main_sidebar(checklist_option):
             # Map
             html.Div(
                 [
-                    html.H2("Location Map"),
+                    html.H3("Location Map"),
                     html.P(id="map-text"),
                     html.P(id="graph-text-param", style={"font-weight": "bold", "text-align": "center"}),
                     html.Div(id="map-tab-graph", className="map-view-container"),
