e3f6f76559a78794e611df889a5de570c37aa5f0
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-09-15 13:52:57 -0600

    format and add more css files

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 # app.py
 """
-This is the entry point of the web application. The program needs to import the callbacks 
-to the main application for dash to find them in the global environment.  The callbacks rely on data.
-That data is downloaded at the start of the program when callbacks is imported.  Data is imported when the callbacks
-module imports the data module.  
+This is the entry point of the web application. The program needs to import the callbacks
+to the main application for dash to find them in the global environment.
+The callbacks rely on data. That data is downloaded at the start of
+the program when callbacks is imported. Data is imported when the callbacks
+module imports the data module.
 """
 import dash
 import dash_bootstrap_components as dbc
@@ -14,12 +15,32 @@ import utils.data
 import utils.callbacks
 from components.layout import make_layout
 
+CSS = [
+    # dbc.themes.SPACELAB,
+    "assets/css/common.css",
+    "assets/css/custom.css",
+    "assets/css/03_style.css",
+    # "assets/uswds/dist/css/uswds.css"
+]
 
 f_app = flask.Flask(__name__)
 # Initialize App
-app = dash.Dash(__name__, assets_folder="assets", external_stylesheets=[dbc.themes.SPACELAB], title=APP_TITLE, server=f_app)  # type: ignore  # pylance doesn't think f_app is suitable.
+app = dash.Dash(
+    __name__,
+    assets_folder="assets",
+    external_stylesheets=CSS,
+    title=APP_TITLE,
+    server=f_app,
+)  # type: ignore  # pylance doesn't think f_app is suitable.
 # server = app.server  # Important for debugging and using Flask!
+
+
 app.layout = make_layout()
 
 if __name__ == "__main__":
-    app.run_server(debug=False, port=PORT, host=ADDRESS)
+    app.run_server(
+        debug=True,
+        port=PORT,
+        host=ADDRESS,
+        threaded=True,
+    )
