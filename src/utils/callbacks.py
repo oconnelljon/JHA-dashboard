9bd74e0312fded90f8e8abf17ecba66ade330547
O'Connell, Jonathan P <joconnell@usgs.gov>
2023-09-15 13:53:45 -0600

    more graphs

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -53,6 +53,12 @@ def summarize_data(mem_data, param) -> tuple:
     table_median = group_staid["ResultMeasureValue"].median().round(3)
     table_median = table_median.rename("Median Value")
 
+    table_min = group_staid["ResultMeasureValue"].min().round(3)
+    table_min = table_min.rename("Min Value")
+
+    table_max = group_staid["ResultMeasureValue"].max().round(3)
+    table_max = table_max.rename("Max Value")
+
     last_sample = group_staid["ActivityStartDate"].max()
     last_sample = last_sample.rename("Latest Sample")
 
@@ -60,7 +66,17 @@ def summarize_data(mem_data, param) -> tuple:
     first_sample = first_sample.rename("First Sample")
 
     my_data = pd.concat(
-        [station_nms, total_samples, non_detects, table_median, first_sample, last_sample], axis=1
+        [
+            station_nms,
+            total_samples,
+            non_detects,
+            table_median,
+            table_min,
+            table_max,
+            first_sample,
+            last_sample,
+        ],
+        axis=1,
     )
 
     my_data["Station ID"] = my_data.index
@@ -73,6 +89,8 @@ def summarize_data(mem_data, param) -> tuple:
             "Sample Count",
             "Not Detected",
             "Median Value",
+            "Min Value",
+            "Max Value",
         ]
     ]
     my_data = my_data.sort_values(
@@ -103,22 +121,22 @@ def sync_checklists(staids_selected, all_selected):
     return staids_selected, all_selected
 
 
-@dash.callback(
-    Output("download-dataframe-csv", "data"),
-    Input("download-modal-button", "n_clicks"),
-    prevent_initial_call=True,
-)
-def user_download(n_clicks):
-    return dcc.send_data_frame(data.ALL_DATA_DF.to_csv, "You_data.csv")
+# @dash.callback(
+#     Output("download-dataframe-csv", "data"),
+#     Input("download-modal-button", "n_clicks"),
+#     prevent_initial_call=True,
+# )
+# def user_download(n_clicks):
+#     return dcc.send_data_frame(data.ALL_DATA_DF.to_csv, "You_data.csv")
 
 
-@dash.callback(
-    Output("download-modal-container", "is_open"),
-    [Input("download-button", "n_clicks"), Input("cancel-button", "n_clicks")],
-    [State("download-modal-container", "is_open")],
-)
-def toggle_modal(n1, n2, is_open):
-    return not is_open if n1 or n2 else is_open
+# @dash.callback(
+#     Output("download-modal-container", "is_open"),
+#     [Input("download-button", "n_clicks"), Input("cancel-button", "n_clicks")],
+#     [State("download-modal-container", "is_open")],
+# )
+# def toggle_modal(n1, n2, is_open):
+#     return not is_open if n1 or n2 else is_open
 
 
 @dash.callback(
@@ -214,7 +232,8 @@ def filter_scatter_data(station_nm, start_date, end_date, param_x, param_y):
     ],
 )
 def map_view_map(mem_data, checklist, param, end_date):
-    # This is technically a secret, but anyone can request this from mapbox so I'm not concerened about it.
+    # This is technically a secret, but anyone can request
+    # this from mapbox so I'm not concerened about it.
     mem_df = pd.read_json(mem_data)
     mem_df = mem_df.sort_values(by="datetime")
     mem_df = mem_df.drop_duplicates(subset="staid", keep="last")
@@ -325,7 +344,7 @@ def map_view_map(mem_data, checklist, param, end_date):
         or mem_df["ResultMeasure/MeasureUnitCode"]
         .loc[~mem_df["ResultMeasure/MeasureUnitCode"].isnull()]
         .empty
-    ):  #  or bool(date_filtered_mem_df["ResultMeasure/MeasureUnitCode"].isnull().array[0])
+    ):
         color_bar_title = ""
     else:
         color_bar_title = (
@@ -414,7 +433,7 @@ def plot_param_ts(mem_data, param):
     )
 
     fig.update_layout(
-        margin=dict(l=5, r=5, t=5, b=5),
+        margin=dict(l=5, r=5, t=30, b=5),
         xaxis_title="Sample Date",
         yaxis_title=data.available_param_dict.get(param),
     )
@@ -470,35 +489,72 @@ def plot_dumbbell(mem_data, param):
 
     long_nones = [None] * len(line_data_x)
     line_data_y = list(common.roundrobin(staids, staids, long_nones))
+    plotting_df = pd.DataFrame(
+        {
+            "ResultMeasureValue_first": first_values,
+            "ResultMeasureValue_last": last_values,
+            "station_nm": staids,
+        },
+    )
+    plotting_df = pd.merge(plotting_df, min_df, on="station_nm")
+    plotting_df = pd.merge(plotting_df, max_df, on="station_nm")
+    start_custom_data = np.stack(
+        (
+            plotting_df["station_nm"],
+            plotting_df["ActivityStartDate_x"],
+            plotting_df["staid_x"],
+            plotting_df["ValueAndUnits_x"],
+        ),
+        axis=-1,
+    )
+    end_custom_data = np.stack(
+        (
+            plotting_df["station_nm"],
+            plotting_df["ActivityStartDate_y"],
+            plotting_df["staid_y"],
+            plotting_df["ValueAndUnits_y"],
+        ),
+        axis=-1,
+    )
+    hover_template = "Station Name: %{customdata[0]}<br>Datetime: %{customdata[1]}<br>Station ID: %{customdata[2]}<br>Result: %{customdata[3]}<br><extra></extra>"
 
     fig = go.Figure(
         data=[
             go.Scatter(
-                x=first_values,
-                y=staids,
+                x=plotting_df["ResultMeasureValue_first"],
+                y=plotting_df["station_nm"],
                 name="First sample",
                 mode="markers",
                 marker=dict(
                     color="#B2D0EF",
                     size=16,
                 ),
+                customdata=start_custom_data,
+                hovertemplate=hover_template,
             ),
             go.Scatter(
-                x=last_values,
-                y=staids,
+                x=plotting_df["ResultMeasureValue_last"],
+                y=plotting_df["station_nm"],
                 name="Last sample",
                 mode="markers",
                 marker=dict(
                     color="#3283D6",
                     size=16,
                 ),
+                customdata=end_custom_data,
+                hovertemplate="Station Name: %{customdata[0]}<br>"
+                + "Datetime: %{customdata[1]}<br>"
+                + "Station ID: %{customdata[2]}<br>"
+                + "Result: %{customdata[3]}<br>"
+                + "<extra></extra>",
             ),
-            # Plot lines and arrows
+            # # Plot lines and arrows
             go.Scatter(
                 x=line_data_x,
                 y=line_data_y,
                 mode="markers+lines",
                 showlegend=False,
+                hoverinfo="skip",
                 marker=dict(
                     symbol="arrow",
                     color="black",
@@ -573,7 +629,7 @@ def plot_box(mem_data, param):
 
     fig.update_layout(
         yaxis_title=str(data.available_param_dict.get(param)),
-        margin=dict(l=5, r=5, t=5, b=5),
+        margin=dict(l=5, r=5, t=30, b=5),
     )
     return fig
 
@@ -590,32 +646,18 @@ def plot_xy(mem_data, param_x: str, param_y: str):
     mem_df = pd.read_json(mem_data)
     x_data = mem_df.loc[mem_df["USGSPCode"] == param_x]
     y_data = mem_df.loc[mem_df["USGSPCode"] == param_y]
-    # y_data = y_data.loc[:,["staid", "datetime", "ResultMeasureValue", "USGSPCode"]]  # Can take out later, just helping debug now.
     combined = pd.merge(x_data, y_data, on="datetime")
     combined = combined.dropna(subset=["ResultMeasureValue_x", "ResultMeasureValue_y"])
     combined.rename(columns={"staid_x": "staid"}, inplace=True)
     combined.rename(columns={"station_nm_x": "station_nm"}, inplace=True)
-    # combined_x = combined["ResultMeasureValue_x"].array
-    # combined_y = combined["ResultMeasureValue_y"].array
-    # combined_color = "station_nm"
-    # if combined.empty:
-    #     combined_color = array("f", [1.0])
-    #     combined_x = array("f", [float("NaN")])
-    #     combined_y = array("f", [float("NaN")])
 
-    fig = go.Figure(
-        layout=dict(template="plotly")
-    )  # !important!  Solves strange plotly bug where graph fails to load on initialization,
+    # !important!  Solves strange plotly bug where graph fails to load on initialization
+    fig = go.Figure(layout=dict(template="plotly"))
     fig = px.scatter(
         combined,
         x="ResultMeasureValue_x",
         y="ResultMeasureValue_y",
         color="station_nm",
-        # """
-        # Need to work on labels.
-        # Look for more SMCLs
-        # other graphs?
-        # """
         labels={
             "station_nm": "Station Name",
             "datetime": "Sample Date",
@@ -655,7 +697,7 @@ def plot_xy(mem_data, param_x: str, param_y: str):
         y_title = common.title_wrapper(y_title)
 
     fig.update_layout(
-        margin=dict(l=5, r=5, t=5, b=5),
+        margin=dict(l=5, r=5, t=30, b=5),
         title_x=0.5,
         xaxis_title=x_title,
         yaxis_title=y_title,
@@ -769,7 +811,7 @@ def plot_xyz(checklist, start_date, end_date, param_x: str, param_y: str, param_
 
     fig.update_layout(
         # title="Comparative Parameter Plot",
-        margin=dict(l=5, r=5, t=5, b=5),
+        margin=dict(l=5, r=5, t=30, b=5),
         title_x=0.5,
         xaxis_title=x_title,
         yaxis_title=y_title,
